63d50b8056e198adfdd2acc2dc3b09406b13e9f1
deyversonsilvaab-cmd <deyversonsilvaab@gmail.com>
2026-06-25 21:00:49 -0300

    Add files via upload

---
+++ tests/test-payload.js
@@ -0,0 +1,8 @@
+console.log(JSON.stringify({
+  name: "Cliente Teste",
+  phone: "17999999999",
+  message: "Preciso de 100m cabo 10mm e 2 disjuntores bipolar 40a",
+  items: "[]",
+  summary: "",
+  lastIntent: ""
+}, null, 2));
