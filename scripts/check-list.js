165d5793cd439c3290ad9d1e95396079fb487926
deyversonsilvaab-cmd <deyversonsilvaab@gmail.com>
2026-06-26 17:35:57 -0300

    Delete scripts/check-list.js

--- scripts/check-list.js
+++
@@ -1,4 +0,0 @@
-import { loadProducts, searchProducts } from "../lib/products.js";
-
-console.log(`Produtos carregados: ${loadProducts().length}`);
-console.log(searchProducts(process.argv.slice(2).join(" ") || "cabo 10mm", 10));
